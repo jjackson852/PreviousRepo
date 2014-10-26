011e9ac53da427cda3cb9fd8b5f1bb57d82fc316
James Jackson <jjackson852@yahoo.com>
2014-10-26 18:13:51 -0500

    First test commit.

---
+++ src/com/parse/package1/ConnectaTechActivity1.java
@@ -0,0 +1,23 @@
+package com.parse.package1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.parse.package1.ConnectaTechApp;import com.parse.ParseAnalytics;
+import com.parse.ParseObject;import com.parse.package1.R;
+
+public class ConnectaTechActivity1 extends Activity {
+	/** Called when the activity is first created. */
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		ParseAnalytics.trackAppOpened(getIntent());
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+
+	}
+}
---
+++ src/com/parse/package1/ConnectaTechApp.java
@@ -0,0 +1,28 @@
+package com.parse.package1;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
+public class ConnectaTechApp extends Application {
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+
+    // Add your initialization code here
+    Parse.initialize(this, "KDRvsx2RMM1vqjlfCOgTd825x82kjWwhaazQRtbm", "7C6CUyoUxT0mv2f0IKQqrafVB2AZXjaSdAtvv8oe");
+
+
+    ParseUser.enableAutomaticUser();
+    ParseACL defaultACL = new ParseACL();
+      
+    // If you would like all objects to be private by default, remove this line.
+    defaultACL.setPublicReadAccess(true);
+    
+    ParseACL.setDefaultACL(defaultACL, true);
+
+  }
+}
