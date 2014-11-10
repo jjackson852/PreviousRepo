aa9c3a49dcb931af251a5eeaab068549053e1fc2
James Jackson <jjackson852@yahoo.com>
2014-11-09 20:34:17 -0600

    11/09/2014 8:34

--- src/com/parse/package1/ConnectaTechActivity1.java
+++ src/com/parse/package1/ConnectaTechActivity1.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
 
 public class ConnectaTechActivity1 extends Activity {
     int i = 0;
+    EditText mEdit;
+
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,12 +22,18 @@ public class ConnectaTechActivity1 extends Activity {
 
 
         Button mButton;
+
+
+
         mButton = (Button)findViewById(R.id.button1);
         mButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                mEdit   = (EditText)findViewById(R.id.editText);
+
                 ParseObject customer = new ParseObject("Customer");
-                customer.put("firstName", "James");
+                customer.put("firstName", mEdit.getText().toString());
                 customer.put("lastName", "Jackson" + i);
+
                 customer.saveInBackground();
                 i++;
 
