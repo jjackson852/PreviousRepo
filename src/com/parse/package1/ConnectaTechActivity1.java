c67fd3425f69ce49da6e215df3767f4ff3db6b2d
Adam Jordan <jordanadamc@gmail.com>
2014-11-17 19:00:56 -0600

    Login Screen Example Simple proof of concept login screen added http://www.tutorialspoint.com/android/android_login_screen.htm

--- src/com/parse/package1/ConnectaTechActivity1.java
+++ src/com/parse/package1/ConnectaTechActivity1.java
@@ -1,29 +1,39 @@
 package com.parse.package1;
 
 
+
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.graphics.Color;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import com.parse.ParseAnalytics;
-import com.parse.ParseObject;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ConnectaTechActivity1 extends Activity {
-    int i = 0;
-    EditText mEdit;
+    private EditText  username=null;
+    private EditText  password=null;
+    private TextView attempts;
+    private Button login;
+    int counter = 3;
 
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        username = (EditText) findViewById(R.id.editText1);
+        password = (EditText) findViewById(R.id.editText2);
+        attempts = (TextView) findViewById(R.id.textView5);
+        attempts.setText(Integer.toString(counter));
+        login = (Button) findViewById(R.id.button1);
+    }
+/*		ParseAnalytics.trackAppOpened(getIntent());
 
-		ParseAnalytics.trackAppOpened(getIntent());
+  //      Button mButton;
 
-        Button mButton;
-
-        mButton = (Button)findViewById(R.id.button3);
+    //    mButton = (Button)findViewById(R.id.button3);
         mButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 mEdit   = (EditText)findViewById(R.id.editText);
@@ -35,11 +45,29 @@ public class ConnectaTechActivity1 extends Activity {
 //test2
                 customer.saveInBackground();
                 i++;
-
+*/
+    public void login(View view){
+        if(username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")){
+            Toast.makeText(getApplicationContext(), "Redirecting...",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+            attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if(counter==0){
+                login.setEnabled(false);
             }
-        });
+
+        }
+        }
+
+
+    }
 
 
 
-	}
-}
+
